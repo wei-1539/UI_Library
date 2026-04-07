237a2a24ed4b2b66db917e0a5aa42aa036110d00
Rio Wei <wutony0713@gmail.com>
2026-04-07 17:07:46 +0800

    feat: 排版建立

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <header>
+      <h1>Header</h1>
+    </header>
+    <main>
+      <slot />
+    </main>
+    <footer>
+      <h1>Footer</h1>
+    </footer>
+  </div>
+</template>

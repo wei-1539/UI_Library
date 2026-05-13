1205cb0a499128f3d4fb38375d299a483a321fab
Rio Wei <wutony0713@gmail.com>
2026-05-14 00:03:28 +0800

    feat: stroybook引入 composables 共用方法路徑

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,9 @@ import type { StorybookConfig } from '@storybook-vue/nuxt';
 const config: StorybookConfig = {
   stories: [
     "../components/**/*.mdx",
-    "../components/**/*.stories.@(js|jsx|ts|tsx|mdx)"
+    "../components/**/*.stories.@(js|jsx|ts|tsx|mdx)",
+    "../composables/**/*.mdx",
+    "../composables/**/*.stories.@(js|jsx|ts|tsx|mdx)",
   ],
   addons: [
     "@storybook/addon-a11y",
